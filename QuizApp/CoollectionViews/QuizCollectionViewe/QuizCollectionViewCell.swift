46e33cc6264598a62163ddef3e6b3d98e8ae375b
botanPro <botanamedi8@gmail.com>
2024-09-28 19:02:47 +0300

    Last PDK One Sat 28

---
+++ QuizApp/CoollectionViews/QuizCollectionViewe/QuizCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  QuizCollectionViewCell.swift
+//  QuizApp
+//
+//  Created by Botan Amedi on 21/09/2024.
+//
+
+import UIKit
+
+class QuizCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ QuizApp/TableViewCells/SearchByTagsTableViewCell/SearchByTagsTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  SearchByTagsTableViewCell.swift
+//  QuizApp
+//
+//  Created by Botan Amedi on 28/09/2024.
+//
+
+import UIKit
+
+class SearchByTagsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
